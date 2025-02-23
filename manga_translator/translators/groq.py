0fd8328c791783d074dece9d61914a5b1dbac11c
DuckDuckGo-prog <weql4143@duck.com>
2025-02-24 04:21:08 +0800

    fix errrors test

--- manga_translator/translators/groq.py
+++ manga_translator/translators/groq.py
@@ -50,8 +50,8 @@ class GroqTranslator(CommonTranslator):
 
     _CHAT_SAMPLE = [
     (
-        'Translate the following text into English and return the result strictly in JSON format.\n\n{"untranslated": "<|1|>恥ずかしい… 目立ちたくない… 私が消えたい…<|2|>きみ… 大丈夫⁉<|3|>なんだこいつ 空気読めて ないのか…？"}',
-        '\n{"translated": "<|1|>So embarrassing… I don’t want to stand out… I wish I could disappear…<|2|>Hey… Are you okay!?<|3|>What’s with this person? Can’t they read the room…?"}\n'
+        'Translate the following text into English and return the result strictly in JSON format.\n\n{"untranslated": "<|1|>恥ずかしい… 目立ちたくない… 私が消えたい…\\n<|2|>きみ… 大丈夫⁉\\n<|3|>なんだこいつ 空気読めて ないのか…？"}',
+        '\n{"translated": "<|1|>So embarrassing… I don’t want to stand out… I wish I could disappear…\\n<|2|>Hey… Are you okay!?\\n<|3|>What’s with this person? Can’t they read the room…?"}\n'
     )
     ]
 
