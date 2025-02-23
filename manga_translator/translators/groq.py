33363125370955cadf09144ee32c768b9ad9ea91
DuckDuckGo-prog <weql4143@duck.com>
2025-02-24 07:01:43 +0800

    Revert "fix for /n and backlashes"

--- manga_translator/translators/groq.py
+++ manga_translator/translators/groq.py
@@ -161,5 +161,4 @@ class GroqTranslator(CommonTranslator):
             
         # Clean up the response
         cleaned_content = content.replace("{'translated':'", '').replace('}', '').replace("\\'", "'").replace("\\\"", "\"").strip("'{}")
-        cleaned_content = cleaned_content.replace("\n", "")
         return cleaned_content
